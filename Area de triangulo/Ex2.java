0527ea584204483b59e2d645016f42d90f37fdc4
joao_junio <joaojunio818@gmail.com>
2025-02-20 21:05:10 -0300

    Introdução

---
+++ Area de triangulo/Ex2.java
@@ -0,0 +1,10 @@
+public class Ex2 {
+  public static void main(String[] args) {
+
+    int altura = 4, largura = 6, area;
+    area = altura * largura;
+
+    System.out.println("A área é: " + area);
+    System.out.println("Altura : " + altura + ", Largura : " + largura);
+  }
+}
---
+++ Tipos de dados/Ex1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Ex1 {
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+    System.out.println("Estou aprendendo Java");
+    System.out.println("50" + "50");
+    System.out.println(50 + 50);
+
+    String nome = "João Junio";
+    int idade = 16;
+    double peso = 500.5;
+    boolean estudandoJava = true;
+
+    final int NUMERO = 10;
+
+    System.out.println("Nome: " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("Peso: " + peso);
+    System.out.println("Estou estudando java? " + estudandoJava);
+    System.out.println("Número: " + NUMERO);
+  }
+}
